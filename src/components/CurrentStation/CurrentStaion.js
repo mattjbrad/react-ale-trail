d2083c7141ebe1d3c0f1a75a4219bde411612b7f
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-06-05 16:58:14 +0100

    fixing select box

--- src/components/CurrentStation/CurrentStaion.js
+++ src/components/CurrentStation/CurrentStaion.js
@@ -4,16 +4,16 @@ const currentStation = (props) => {
   return (
     <div>
       <p>You are currently in {props.current}</p>
-      <select onChange={props.change} name="currentStation">
-        <option value='syb'>Staylybridge</option>
-        <option value='gnf'>Greenfield</option>
-        <option value='msn'>Marsden</option>
-        <option value='swt'>Slaithwaite</option>
-        <option value='hud'>Huddersfield</option>
-        <option value='mir'>Mirfield</option>
-        <option value='dew'>Dewsbury</option>
-        <option value='btl'>Batley</option>
-    </select>
+        <select onChange={props.change} name="currentStation">
+          <option value='syb'>Staylybridge</option>
+          <option value='gnf'>Greenfield</option>
+          <option value='msn'>Marsden</option>
+          <option value='swt'>Slaithwaite</option>
+          <option value='hud'>Huddersfield</option>
+          <option value='mir'>Mirfield</option>
+          <option value='dew'>Dewsbury</option>
+          <option value='btl'>Batley</option>
+      </select>
     </div>
   )
 }
